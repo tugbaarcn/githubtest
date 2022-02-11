915925c37cf253935fe4803ab3d46461c43e067c
Tuğba Arıcan <tugbaaricaan@gmail.com>
2022-02-11 22:32:09 +0300

    My  update

--- src/com/company/Second_Class.java
+++ src/com/company/Second_Class.java
@@ -5,6 +5,7 @@ public class Second_Class {
     public static void main(String[] args) {
 
         System.out.println("new page second class");
+        System.out.println("How are you");
 
 
     }
