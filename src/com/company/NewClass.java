da1d1c876554a6a1e33ac79f6e1c900f8b2bff06
Tuğba Arıcan <93740743+tugbaarcn@users.noreply.github.com>
2022-02-11 22:52:41 +0300

    Update NewClass.java

--- src/com/company/NewClass.java
+++ src/com/company/NewClass.java
@@ -3,6 +3,8 @@ package com.company;
 public class NewClass {
     public static void main(String[] args) {
         add new codes
+            String a = "Java";
+            int b = 9;  
 
     }
 }
