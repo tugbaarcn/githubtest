8a79be739b190a32afdb62cb9ee7f9f744b6ae9a
Tuğba Arıcan <tugbaaricaan@gmail.com>
2022-02-11 22:57:29 +0300

    My new class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("1st class");
+
         System.out.println("2nd step");
+
         System.out.println("3rd step");
+
         System.out.println("4th step");
+
         System.out.println("5th step");
+
         System.out.println();
 
     }
